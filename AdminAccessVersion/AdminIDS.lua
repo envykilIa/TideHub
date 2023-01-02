1697d842f5ed5ca5c8f6a6a807221c0aa381fb68
envykilIa <113392869+envykilIa@users.noreply.github.com>
2023-01-02 15:31:29 -0800

    Update AdminIDS.lua

--- AdminAccessVersion/AdminIDS.lua
+++ AdminAccessVersion/AdminIDS.lua
@@ -1,1 +1,13 @@
--- Real
+DevIDS = {
+    2688454949, -- fortunatesouIs
+    3503576859, -- fortunatesouIs [ALT #1]
+}
+
+
+return DevIDS
+
+if game.Players.LocalPlayer.UserId == DevIDS then
+    pcall(function()
+        -- Admin Loadstring Soon
+    end
+end)
