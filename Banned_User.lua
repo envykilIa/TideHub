955965dbec23e39b38bc4818c81de08a0cb000a4
envykilIa <113392869+envykilIa@users.noreply.github.com>
2023-01-08 15:21:14 -0800

    Update Banned_User.lua

--- Banned_User.lua
+++ Banned_User.lua
@@ -2,7 +2,7 @@ local banned = {4024419747,}
 for _,v in pairs(game.Players:GetDescendants()) do
     if table.find(banned, v.UserId) then
         if v.Character then
-            game.Players.LocalPlayer:Kick("Banned From Tide Hub [FREE]")
+            game.Players.LocalPlayer:Kick("Banned From Tide Hub")
         end
     end
 end
