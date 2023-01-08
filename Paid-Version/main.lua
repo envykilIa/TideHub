e73d29e71af2ceb06a99353dcc7a029a5565b7c2
envykilIa <113392869+envykilIa@users.noreply.github.com>
2023-01-08 15:22:17 -0800

    Create main.lua

---
+++ Paid-Version/main.lua
@@ -0,0 +1,6 @@
+pcall(function()
+    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/envykilIa/TideHub/main/Banned_User.lua"))()
+end)
+
+-- why are you here?
+print('you are a skidde..')
